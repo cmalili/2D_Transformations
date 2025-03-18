6d2175e5add64fa578085ea256142fd446361c2d
Christopher, Malili <malilichristopher@gmail.com>
2025-03-18 01:23:15 -0700

    test of custom detector created

---
+++ custom_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 17 21:47:03 2025
+
+@author: cmalili
+"""
+
+
+from transformations_c_recovery import demo_transform_recovery
+
+image_path = "rec1.png"
+image_path2 = "rec1_transform1.png"
+
+demo_transform_recovery(image_path, image_path2)
